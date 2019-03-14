ed01ca5d14840361bf00917af21fecb7165624ed
graphicskart <graphicskart2018@gmail.com>
2019-03-14 07:41:39 +0530

    Updates

---
+++ precache-manifest.6ba71eae24d2a6a82c1f8951db0c4fd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59bb6d4525e3ce6ca483",
+    "url": "/bootstrap-pricing-table/static/js/runtime~main.07ace2d5.js"
+  },
+  {
+    "revision": "c070b82c86c2e08a3bfb",
+    "url": "/bootstrap-pricing-table/static/js/main.181b35b3.chunk.js"
+  },
+  {
+    "revision": "df669b1de5560294517f",
+    "url": "/bootstrap-pricing-table/static/js/2.92d63167.chunk.js"
+  },
+  {
+    "revision": "c070b82c86c2e08a3bfb",
+    "url": "/bootstrap-pricing-table/static/css/main.9bdf8700.chunk.css"
+  },
+  {
+    "revision": "d563aa6cae862d10bfce40231d8d1cb5",
+    "url": "/bootstrap-pricing-table/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/bootstrap-pricing-table/precache-manifest.6ba71eae24d2a6a82c1f8951db0c4fd6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/bootstrap-pricing-table/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
